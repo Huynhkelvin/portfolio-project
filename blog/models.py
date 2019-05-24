480ae77b79caf76d7259557c053f445e6a022a2a
unknown <huynhkelvin90@gmail.com>
2019-05-23 23:03:27 -0700

    added static and blogs

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,12 @@ class Blog(models.Model):
     pub_date = models.DateTimeField(null=True, blank=True)
     body = models.TextField(null=True, blank=True)
     image = models.ImageField(upload_to='images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+        
+    def summary(self):
+        return self.body[:100]
+
+    def date(self):
+        return self.pub_date.strftime(' %b, %e %Y')
