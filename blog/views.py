0f05270ae26710091105e19ab81c67238213fbbc
Huynhkelvin <36218412+Huynhkelvin@users.noreply.github.com>
2024-04-23 14:56:57 -0700

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,9 @@ def allblogs(request):
 def detail(request,blog_id):
     detailblog = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'blog/detail.html', {'blog':detailblog})
+
+
+
+
+
+
